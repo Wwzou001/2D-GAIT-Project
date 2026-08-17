b9fe7d16e8139b496a5b5026b455fe496f61ed23
Bradley Shears <s3997190@student.rmit.edu.au>
2026-08-17 11:45:19 +1000

    Player + WASD Player Controls

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem; 
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 4f;
+    private Rigidbody2D rb;
+    private Vector2 movementInput;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = movementInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context){
+        
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    
+
+
+}
